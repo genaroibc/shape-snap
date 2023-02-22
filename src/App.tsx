6a96861c75d652c45f262e48f54dcb315b1e7985
genaroibc <bonavitaismael@gmail.com>
2023-02-22 15:06:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Shape Snap</h1>
+      <h2>One tool, all social media platforms</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ todo.md
@@ -0,0 +1,5 @@
+"One tool, all social media platforms."
+
+"snap, shape, share"
+
+- nonrelated: add a feature to copy code blocks on ikodi
