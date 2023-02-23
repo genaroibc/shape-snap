54fa762c41452fb4ef1086c84a120ab5e9a5c59a
genaroibc <bonavitaismael@gmail.com>
2023-02-23 17:18:38 -0300

    [FEAT]: save user image in state and display it at the end

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import { PlatformList } from './PlatformList';
 import { UploadZone } from './UploadZone';
+import type { ImageData } from '../types';
 
-const STEPS_TITLES = ['Upload your images', 'Select social media platforms', 'Customize'];
+const STEPS_TITLES = ['Upload your images', 'Select social media platforms', 'Confirm your choices'];
 const TOTAL_STEPS = STEPS_TITLES.length;
 
 export function Steps() {
   const [currentStep, setCurrentStep] = useState(0);
   const [selectedPlatforms, setSelectedPlatforms] = useState<Record<string, boolean>>({});
+  const [userImgData, setUserImgData] = useState<ImageData | null>(null);
 
   const handleSelectionChange = (e: React.ChangeEvent) => {
     const platformInput = e.target as HTMLInputElement;
@@ -17,6 +19,10 @@ export function Steps() {
     if (!platformName) return;
 
     setSelectedPlatforms((currentPlatforms) => ({ ...currentPlatforms, [platformName]: isSelectedPlatform }));
+  };
+
+  const handleUserImage = (imageData: ImageData) => {
+    setUserImgData(imageData);
   };
 
   return (
@@ -32,12 +38,22 @@ export function Steps() {
           </span>
         ))}
       </nav>
-      <progress value={currentStep} max={TOTAL_STEPS} className="w-full" />
+      <progress value={currentStep} max={TOTAL_STEPS - 1} className="w-full" />
       <h3 className="text-4xl my-8">{STEPS_TITLES[currentStep]}</h3>
-      {currentStep === 0 && <UploadZone />}
+      {currentStep === 0 && <UploadZone onUserImage={handleUserImage} />}
       {currentStep === 1 && <PlatformList onSelectionChange={handleSelectionChange} />}
-      {currentStep === 2 && <h5>another</h5>}
-      {currentStep === 3 && <h5>another</h5>}
+      {currentStep === 2 && (
+        <div className="max-w-xs w-full mx-auto text-center flex flex-col gap-4">
+          {userImgData && (
+            <img className="border-green-500 border-4 rounded-md" src={userImgData.src} alt={userImgData.title} />
+          )}
+          {Object.entries(selectedPlatforms).map(([name, isSelected]) => (
+            <span className="font-bold" key={name}>
+              {isSelected && name}
+            </span>
+          ))}
+        </div>
+      )}
 
       {currentStep > 0 && <button onClick={() => setCurrentStep((p) => (p > 1 ? p - 1 : 0))}>prev</button>}
       {currentStep < TOTAL_STEPS && (
@@ -45,7 +61,6 @@ export function Steps() {
       )}
 
       <button disabled={currentStep !== TOTAL_STEPS}>Generate images</button>
-      {JSON.stringify(selectedPlatforms)}
     </section>
   );
 }
