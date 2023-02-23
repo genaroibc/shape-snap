7b4a27ee2917b8819e989e09262886ec180090c1
genaroibc <bonavitaismael@gmail.com>
2023-02-23 17:09:52 -0300

    [DEV]: remove upload photo responsibility

--- src/components/UploadZone.tsx
+++ src/components/UploadZone.tsx
@@ -1,37 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { readImageFile } from '../utils/readImageFile';
-import axios from 'axios';
+import type { ImageData } from '../types';
 
-export function UploadZone() {
-  const [userImg, setUserImg] = useState<{ src: string; title: string } | null>(null);
+type Props = {
+  onUserImage: (imageData: ImageData) => void;
+};
+
+export function UploadZone({ onUserImage: onUserImage }: Props) {
+  const [userImg, setUserImg] = useState<ImageData | null>(null);
   const [error, setError] = useState<string | null>(null);
-  const [transformedImgUrl, setTransformedImgUrl] = useState('');
   const [isDragging, setIsDragging] = useState(false);
 
-  const uploadImageFile = async (imageData: File) => {
-    const API_URL = import.meta.env.VITE_CLOUDINARY_UPLOAD_API_URL;
-
-    if (!API_URL) {
-      throw new Error("'VITE_CLOUDINARY_UPLOAD_API_URL' env variable is not defined");
+  useEffect(() => {
+    if (userImg?.src && userImg.title) {
+      onUserImage({ src: userImg?.src, title: userImg?.title });
     }
-
-    const data = new FormData();
-
-    data.append('file', imageData);
-    data.append('upload_preset', 'shape-snap'); // set up your upload_preset on cloudinary.
-
-    try {
-      const res = await axios.post(API_URL, data, {
-        onUploadProgress: (event) => {
-          console.log((event.loaded / (event.total ?? 0)) * 100);
-        }
-      });
-      setTransformedImgUrl(res.data.secure_url);
-      return res.data.secure_url;
-    } catch (error) {
-      return console.error(error);
-    }
-  };
+  }, [userImg]);
 
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
@@ -48,7 +32,6 @@ export function UploadZone() {
         if (!result.ok) return setError(result.error);
 
         setUserImg(result.data);
-        uploadImageFile(imageFile);
       });
     });
   };
@@ -61,7 +44,7 @@ export function UploadZone() {
   };
 
   return (
-    <section className="p-4 flex flex-col gap-4 shadow-2xl">
+    <section className="p-4 flex gap-4 shadow-2xl">
       <section
         draggable
         onDrop={handleDrop}
@@ -74,12 +57,7 @@ export function UploadZone() {
           ${isDragging ? 'bg-green-500 scale-125' : 'bg-transparent'}`}
       >
         <p>upload your image here</p>
-        <img src="/assets/svg/drop.svg" alt="drop here" />
       </section>
-
-      <button className="w-fit m-auto" type="submit">
-        Upload
-      </button>
 
       <div className="max-w-xs w-full mx-auto text-center flex flex-col gap-4">
         {error ? (
@@ -92,9 +70,6 @@ export function UploadZone() {
             </>
           )
         )}
-        {transformedImgUrl && (
-          <img className="border-green-500 border-4 rounded-md" src={transformedImgUrl} alt={'a cat'} />
-        )}{' '}
       </div>
     </section>
   );
