5e2e6527145eeecb98e13feb902eca2c7b5e2899
genaroibc <bonavitaismael@gmail.com>
2023-02-23 09:56:57 -0300

    DEV]: add Hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+export function Hero() {
+  return (
+    <section className="max-w-[1000px] m-auto grid grid-cols-2 gap-4 pt-36">
+      <div>
+        <h1 className="text-5xl font-bold">Shape Snap</h1>
+        <h2 className="text-2xl font-bold mb-8">One tool, all social media platforms</h2>
+
+        <p>Generate profile photos and banners for Twitter, YouTube and Twitch.</p>
+        <button>Try it now</button>
+      </div>
+    </section>
+  );
+}
