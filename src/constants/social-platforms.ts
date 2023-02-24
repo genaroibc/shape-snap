3c6a70a4f710495987127b85ec2d6010f939783d
genaroibc <bonavitaismael@gmail.com>
2023-02-24 15:59:34 -0300

    [DEV]: add platform banner sizes constant

---
+++ src/constants/social-platforms.ts
@@ -0,0 +1,22 @@
+import { PlatformImage } from '../types';
+
+export const PLATFORM_BANNER_SIZES: PlatformImage = {
+  twitter: {
+    banner: {
+      width: 1080,
+      height: 360
+    }
+  },
+  instagram: {
+    banner: null
+  },
+  tiktok: {
+    banner: null
+  },
+  twitch: {
+    banner: null
+  },
+  github: {
+    banner: null
+  }
+};
