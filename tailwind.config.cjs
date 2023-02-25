df0cdb55a16282232decefbdfc4657f58f173ce3
genaroibc <bonavitaismael@gmail.com>
2023-02-25 08:55:23 -0300

    [DEV]: add css variables to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)',
+        'primary-darker': 'var(--primary-color-darker)',
+        secondary: 'var(--secondary-color)',
+        tertiary: 'var(--tertiary-color)',
+        complement: 'var(--complement-color)'
+      }
+    }
   },
   plugins: []
-}
+};
