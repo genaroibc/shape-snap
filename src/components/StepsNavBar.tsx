31ca2c68c50f4d4f9be30ceaa0c272679554e97e
genaroibc <bonavitaismael@gmail.com>
2023-03-02 08:57:34 -0300

    [FEAT]: make StepsNavBar to follow user screen

--- src/components/StepsNavBar.tsx
+++ src/components/StepsNavBar.tsx
@@ -9,8 +9,8 @@ type Props = {
 
 export function StepsNavBar({ currentStep, setCurrentStep, canGoToNextStep }: Props) {
   return (
-    <footer>
-      <nav className="flex gap-4 w-full justify-center p-4">
+    <footer className="sticky bottom-4 bg-black/75 shadow-2xl w-fit mx-auto rounded-2xl">
+      <nav className="flex flex-wrap gap-6 w-full justify-center p-6">
         <button disabled={currentStep < 1} onClick={() => setCurrentStep((p) => (p > 1 ? p - 1 : 0))}>
           prev
         </button>
