75d2af84b9904551e45f324cd8ff433d0b368eda
genaroibc <bonavitaismael@gmail.com>
2023-03-02 08:52:36 -0300

    [FIX]: make Header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,11 @@ export function Header() {
       style={{
         WebkitBackdropFilter: 'blur(8px)'
       }}
-      className="sticky z-40 top-0 left-0 w-full shadow-sm bg-white/80 backdrop-blur-[8px]"
+      className="md:sticky z-40 top-0 left-0 w-full shadow-sm bg-white/80 backdrop-blur-[8px]"
     >
-      <div className="m-auto max-w-[1000px] flex justify-between gap-4 p-4 items-center font-semibold">
+      <div className="m-auto max-w-[1400px] flex flex-col md:flex-row justify-between gap-4 p-4 items-center font-semibold">
         <img src="/assets/logo/transparent-rectangular-logo.svg" alt="Shape Snap logotype" width="150" />
-        <nav className="hidden lg:block">
+        <nav className="block">
           <ul className="flex gap-4 w-full self-end">
             <li>
               <a href="#home" className="font-normal text-gray-800 hover:text-black">
