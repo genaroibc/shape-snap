0e233289d5d2ea7ed7d844676c80c85c900522aa
genaroibc <bonavitaismael@gmail.com>
2023-03-02 09:34:44 -0300

    [FIX]: change order of demo images descriptions

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,12 +7,12 @@ export const TOTAL_STEPS = STEP_LIST.length;
 export const DEMO_IMAGES_LIST = [
   {
     src: '/assets/demo-images/sunrise.jpg',
-    alt: 'A man in the street looking at his back',
+    alt: 'A bird flying in sunrise sky',
     id: uuid()
   },
   {
     src: '/assets/demo-images/man-city-street.jpg',
-    alt: 'A bird flying in sunrise sky',
+    alt: 'A man in the street looking at his back',
     id: uuid()
   }
 ];
