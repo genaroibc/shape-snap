ce6a28541c1ea5d5f8f9a6bea92e6ff221d25548
genaroibc <bonavitaismael@gmail.com>
2023-03-03 17:59:28 -0300

    [FEAT]; add new optimized demo images

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,13 +6,29 @@ export const TOTAL_STEPS = STEP_LIST.length;
 
 export const DEMO_IMAGES_LIST = [
   {
-    src: '/assets/demo-images/sunrise.jpg',
-    alt: 'A bird flying in sunrise sky',
+    src: '/assets/demo-images/butterfly.webp',
+    alt: 'A butterfly pollinating a flower',
     id: uuid()
   },
   {
-    src: '/assets/demo-images/man-city-street.jpg',
-    alt: 'A man in the street looking at his back',
+    src: '/assets/demo-images/woman.webp',
+    alt: 'A woman in a fall forest looking at the camera',
+    id: uuid()
+  },
+
+  {
+    src: '/assets/demo-images/sunset.webp',
+    alt: 'A sunset at sea',
+    id: uuid()
+  },
+  {
+    src: '/assets/demo-images/girl.webp',
+    alt: 'A girl with fireworks behind her',
+    id: uuid()
+  },
+  {
+    src: '/assets/demo-images/mountain.webp',
+    alt: 'A ',
     id: uuid()
   }
 ];
