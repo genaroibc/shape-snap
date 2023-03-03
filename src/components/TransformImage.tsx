eae79c1e8b18ae3fd00325f7507dad5f64c723e9
genaroibc <bonavitaismael@gmail.com>
2023-03-03 19:44:09 -0300

    [FEAT]: style download banners button

--- src/components/TransformImage.tsx
+++ src/components/TransformImage.tsx
@@ -90,7 +90,25 @@ export function TransformImage({ imageData, platformList }: Props) {
 
       {uploadProgress && !transformedImages && <progress className="w-full" max={100} value={uploadProgress} />}
 
-      {transformedImages && <button onClick={handleDownloadZIPFile}>Download generated banners</button>}
+      {transformedImages && (
+        <button className="max-w-fit flex gap-2 items-center justify-center mx-auto" onClick={handleDownloadZIPFile}>
+          Download banners
+          <svg className="w-8 max-w-full" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              {' '}
+              <path
+                d="M20 15V18C20 19.1046 19.1046 20 18 20H6C4.89543 20 4 19.1046 4 18L4 15M8 11L12 15M12 15L16 11M12 15V3"
+                stroke="#fff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>{' '}
+            </g>
+          </svg>
+        </button>
+      )}
     </section>
   );
 }
