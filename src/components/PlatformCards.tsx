f42196a8cb7f4f7fb7422b47e98a991ee0765a2f
genaroibc <bonavitaismael@gmail.com>
2023-03-04 16:42:53 -0300

    [FEAT]: style PlatformCards component

--- src/components/PlatformCards.tsx
+++ src/components/PlatformCards.tsx
@@ -11,27 +11,28 @@ type Props = {
 
 export function PlatformCards({ cards }: Props) {
   return (
-    <div>
-      <div className="flex flex-col gap-20 md:p-4">
-        {cards.map(({ banner, id, platformName }) => (
-          <div key={id} className="flex flex-col justify-center items-center gap-12 md:p-4 text-center rounded">
-            <h3 className="text-4xl text-blue-500">{platformName}</h3>
-            {platformName === 'linkedin' ? (
-              <LinkedinCard {...{ ...banner, id }} />
-            ) : platformName === 'twitter' ? (
-              <TwitterCard {...{ ...banner, id }} />
-            ) : platformName === 'pinterest' ? (
-              <PinterestCard {...{ ...banner, id }} />
-            ) : platformName === 'twitch' ? (
-              <TwitchCard {...{ ...banner, id }} />
-            ) : platformName === 'youtube' ? (
-              <YouTubeCard {...{ ...banner, id }} />
-            ) : (
-              <p>Nothing to render</p>
-            )}
-          </div>
-        ))}
-      </div>
+    <div className="flex flex-col gap-20 md:p-4">
+      {cards.map(({ banner, id, platformName }) => (
+        <div
+          key={id}
+          className="bg-[#fafafa] md:pt-8 md:pb-16 flex flex-col justify-center items-center gap-4 md:p-4 text-center rounded"
+        >
+          <h3 className="text-4xl font-bold">{platformName?.toUpperCase()}</h3>
+          {platformName === 'linkedin' ? (
+            <LinkedinCard {...{ ...banner, id }} />
+          ) : platformName === 'twitter' ? (
+            <TwitterCard {...{ ...banner, id }} />
+          ) : platformName === 'pinterest' ? (
+            <PinterestCard {...{ ...banner, id }} />
+          ) : platformName === 'twitch' ? (
+            <TwitchCard {...{ ...banner, id }} />
+          ) : platformName === 'youtube' ? (
+            <YouTubeCard {...{ ...banner, id }} />
+          ) : (
+            <p>Nothing to render</p>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
